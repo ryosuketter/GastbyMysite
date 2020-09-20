5945733acea7155ee3d1f49e73c85c1c2ce4c658
ryosuketter <r.shimizu.yale@gmail.com>
2020-09-21 08:23:38 +0900

    change txt

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default () => {
             There is no love sincerer than
             <br /> the love of food.
           </h1>
-          <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
+          <p>これはテストです</p>
         </div>
         <div className="wave">
           <img src="/images/wave.svg" alt="" />
